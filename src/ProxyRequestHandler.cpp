8d730e9203b237be51028ba3a0d2c2fdbef1ee68
farice <farissbahi@gmail.com>
2018-03-02 15:44:41 -0500

    bool

--- src/ProxyRequestHandler.cpp
+++ src/ProxyRequestHandler.cpp
@@ -265,7 +265,7 @@ void ProxyRequestHandler::handleRequest(HTTPServerRequest &req, HTTPServerRespon
         Poco::SharedPtr<CacheResponse> checkResponse = this->staticCache.get(key);
         // TODO: - validation logic
         // add CacheResponseItem call that returns whether item needs to be revalidated to validItem
-        validItem = false;
+        bool validItem = false;
 
         if (!validItem) {
           // TODO: - return updated/same cacheitem depending on result of validate method (in this class)
