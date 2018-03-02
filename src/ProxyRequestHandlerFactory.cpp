1940c4f71bf2d48f6f0a5f31cdb8fb9fe8e019e3
Connor-Grehlinger <cmgrehlinger@gmail.com>
2018-03-01 19:46:21 -0500

    Pushing cache dev to new branch

--- src/ProxyRequestHandlerFactory.cpp
+++ src/ProxyRequestHandlerFactory.cpp
@@ -9,17 +9,29 @@ namespace Net {
 
 ProxyRequestHandlerFactory::ProxyRequestHandlerFactory()
 {
+  LOG(DEBUG) << "************* Created ProxyRequestHandlerFactory ************* " << endl;
+this->factoryCache = new Poco::LRUCache<std::string, std::string>();
+ LOG(DEBUG) << "!!!!!!!!!!!!!!!!!!!!!! Shared Proxy Cache created !!!!!!!!!!!!!!!!!!!!!!!!" << endl;
 }
 
 
 ProxyRequestHandlerFactory::~ProxyRequestHandlerFactory()
 {
+  LOG(DEBUG) << "!!!!!!!!!!!!!!!!!!!!!! Shared Proxy Cache destroyed !!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+  delete this->factoryCache;
 }
 
+  
 ProxyRequestHandler* ProxyRequestHandlerFactory::createRequestHandler(const HTTPServerRequest &)
   {
     LOG(DEBUG) << "Create request handler" << endl;
-    return new ProxyRequestHandler;
+    return new ProxyRequestHandler(this->factoryCache);
   }
+
+    
+ProxyRequestHandler* ProxyRequestHandlerFactory::createRequestHandlerWithCache(const HTTPServerRequest &){
+LOG(DEBUG) << "Create request handler with cache" << endl;
+return new ProxyRequestHandler(this->factoryCache);
+}
 
 }}
--- src/ProxyRequestHandlerFactory.h
+++ src/ProxyRequestHandlerFactory.h
@@ -1,6 +1,10 @@
 #include <Poco/Net/HTTPRequestHandlerFactory.h>
 #include "ProxyRequestHandler.h"
 #include <Poco/SharedPtr.h>
+#include <Poco/UniqueExpireCache.h>
+#include <Poco/LRUCache.h>
+#include <Poco/ExpirationDecorator.h>
+#include <ctime>
 
 using namespace std;
 
@@ -15,7 +19,65 @@ public:
   ProxyRequestHandlerFactory();
   ~ProxyRequestHandlerFactory();
   virtual ProxyRequestHandler* createRequestHandler(const HTTPServerRequest &);
+  virtual ProxyRequestHandler* createRequestHandlerWithCache(const HTTPServerRequest &);
+
   Poco::BasicEvent<const bool> serverStopped;
+
+  /* -Could use expire cache, but may prevent re-validation functionality 
+     -Using LRU cache for now 
+  */
+  Poco::LRUCache<std::string, std::string>* factoryCache;
+  
+  
+
 };
 
+/* Instead of having the cache in the factory, you could also have
+   it has a static member of the ProxyRequestHandler class */
+
+/*
+ class CacheResponse{
+   string responseData;
+   int maxFreshness;
+   int currentFreshness;
+   bool isExpired;
+   // some kind of timer 
+   
+
+
+
+
+ };
+
+
+ class CacheResponseTimer{ // timer for determining expiration 
+   double numSeconds;
+   bool isExpired;
+ public:
+
+ CacheResponseTimer(double seconds):numSeconds(seconds), isExpired(false){
+   
+   void startExpire(double seconds){
+     clock_t startExpireTime = clock();
+     double secondsToExpire = seconds;
+     bool stillValid = true;
+     double elapsedTime; 
+     while (stillValid){
+       elapsedTime = (clock() - startExpireTime) / CLOCKS_PER_SEC;
+       if (elapsedTime >= secondsToExpire){
+	 LOG(DEBUG) << " Time is up " << endl;
+	 stillValid = false;
+	 this->isExpired = true;
+       }
+     }
+   }
+
+
+ };
+
+*/
+ 
 }}
+
+
+
