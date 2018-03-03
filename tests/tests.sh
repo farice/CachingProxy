ed1c6b1d4032243ed6e3fac46065485f515a9612
farice <farissbahi@gmail.com>
2018-03-02 23:55:17 -0500

    fix tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -2,19 +2,19 @@
 echo Proxy server is up
 
 echo Testing basic HTTP...
-curl -v -x http://vcm-3262.vm.duke.edu:12345 http://www.example.com/
-echo Testing concurret HTTP...
-curl -v -x http://vcm-3262.vm.duke.edu:12345 http://www.example.com/ &
-curl -v -x http://vcm-3262.vm.duke.edu:12345 http://www.espn.com/ &
-curl -v -x http://vcm-3262.vm.duke.edu:12345 http://www.nba.com/ &
+curl -v -x http://127.0.0.1:12345 http://www.example.com/
+echo Testing concurrent HTTP...
+curl -v -x http://127.0.0.1:12345 http://www.example.com/ &
+curl -v -x http://127.0.0.1:12345 http://www.espn.com/ &
+curl -v -x http://127.0.0.1:12345 http://www.nba.com/ &
 echo Testing HTTP CONNECT
-curl -v -x http://vcm-3262.vm.duke.edu:12345 https://www.nba.com/ &
-curl -v -x http://vcm-3262.vm.duke.edu:12345 https://www.espn.com/ &
+curl -v -x http://127.0.0.1:12345 https://www.nba.com/ &
+curl -v -x http://127.0.0.1:12345 https://www.espn.com/ &
 echo Testing POST requests
 curl -u farice:farice23 http://texnotes.me/login/
 echo Testing hits in the cache
-curl -v -x http://vcm-3262.vm.duke.edu:12345 http://www.example.com/
-curl -v -x http://vcm-3262.vm.duke.edu:12345 http://www.example.com/
-curl -v -x http://vcm-3262.vm.duke.edu:12345 http://www.example.com/
+curl -v -x http://127.0.0.1:12345 http://www.example.com/
+curl -v -x http://127.0.0.1:12345 http://www.example.com/
+curl -v -x http://127.0.0.1:12345 http://www.example.com/
 echo Proxy log:
 cat ../logs/proxy.log
